d7157f2553b0604857310b2910d8420459a3ec9d
Spoon404 <tom.vegelien@gmail.com>
2018-02-12 09:38:53 +0100

    first commit

---
+++ BitflipperV2/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace BitflipperV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbx128 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbx64 = new System.Windows.Forms.CheckBox();
+            this.cbx32 = new System.Windows.Forms.CheckBox();
+            this.cbx16 = new System.Windows.Forms.CheckBox();
+            this.cbx8 = new System.Windows.Forms.CheckBox();
+            this.cbx4 = new System.Windows.Forms.CheckBox();
+            this.cbx2 = new System.Windows.Forms.CheckBox();
+            this.cbx1 = new System.Windows.Forms.CheckBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lbloutdec = new System.Windows.Forms.Label();
+            this.lbloutbin = new System.Windows.Forms.Label();
+            this.lblouthex = new System.Windows.Forms.Label();
+            this.lbloutasc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbx128
+            // 
+            this.cbx128.AutoSize = true;
+            this.cbx128.Location = new System.Drawing.Point(15, 39);
+            this.cbx128.Name = "cbx128";
+            this.cbx128.Size = new System.Drawing.Size(15, 14);
+            this.cbx128.TabIndex = 0;
+            this.cbx128.UseVisualStyleBackColor = true;
+            this.cbx128.CheckedChanged += new System.EventHandler(this.cbx128_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "128";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "64";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "32";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "16";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(141, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "8";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(198, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(229, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "1";
+            // 
+            // cbx64
+            // 
+            this.cbx64.AutoSize = true;
+            this.cbx64.Location = new System.Drawing.Point(46, 39);
+            this.cbx64.Name = "cbx64";
+            this.cbx64.Size = new System.Drawing.Size(15, 14);
+            this.cbx64.TabIndex = 9;
+            this.cbx64.UseVisualStyleBackColor = true;
+            this.cbx64.CheckedChanged += new System.EventHandler(this.cbx64_CheckedChanged);
+            // 
+            // cbx32
+            // 
+            this.cbx32.AutoSize = true;
+            this.cbx32.Location = new System.Drawing.Point(77, 39);
+            this.cbx32.Name = "cbx32";
+            this.cbx32.Size = new System.Drawing.Size(15, 14);
+            this.cbx32.TabIndex = 10;
+            this.cbx32.UseVisualStyleBackColor = true;
+            this.cbx32.CheckedChanged += new System.EventHandler(this.cbx32_CheckedChanged);
+            // 
+            // cbx16
+            // 
+            this.cbx16.AutoSize = true;
+            this.cbx16.Location = new System.Drawing.Point(108, 39);
+            this.cbx16.Name = "cbx16";
+            this.cbx16.Size = new System.Drawing.Size(15, 14);
+            this.cbx16.TabIndex = 11;
+            this.cbx16.UseVisualStyleBackColor = true;
+            this.cbx16.CheckedChanged += new System.EventHandler(this.cbx16_CheckedChanged);
+            // 
+            // cbx8
+            // 
+            this.cbx8.AutoSize = true;
+            this.cbx8.Location = new System.Drawing.Point(139, 39);
+            this.cbx8.Name = "cbx8";
+            this.cbx8.Size = new System.Drawing.Size(15, 14);
+            this.cbx8.TabIndex = 12;
+            this.cbx8.UseVisualStyleBackColor = true;
+            this.cbx8.CheckedChanged += new System.EventHandler(this.cbx8_CheckedChanged);
+            // 
+            // cbx4
+            // 
+            this.cbx4.AutoSize = true;
+            this.cbx4.Location = new System.Drawing.Point(165, 39);
+            this.cbx4.Name = "cbx4";
+            this.cbx4.Size = new System.Drawing.Size(15, 14);
+            this.cbx4.TabIndex = 13;
+            this.cbx4.UseVisualStyleBackColor = true;
+            this.cbx4.CheckedChanged += new System.EventHandler(this.cbx4_CheckedChanged);
+            // 
+            // cbx2
+            // 
+            this.cbx2.AutoSize = true;
+            this.cbx2.Location = new System.Drawing.Point(196, 39);
+            this.cbx2.Name = "cbx2";
+            this.cbx2.Size = new System.Drawing.Size(15, 14);
+            this.cbx2.TabIndex = 14;
+            this.cbx2.UseVisualStyleBackColor = true;
+            this.cbx2.CheckedChanged += new System.EventHandler(this.cbx2_CheckedChanged);
+            // 
+            // cbx1
+            // 
+            this.cbx1.AutoSize = true;
+            this.cbx1.Location = new System.Drawing.Point(227, 39);
+            this.cbx1.Name = "cbx1";
+            this.cbx1.Size = new System.Drawing.Size(15, 14);
+            this.cbx1.TabIndex = 15;
+            this.cbx1.UseVisualStyleBackColor = true;
+            this.cbx1.CheckedChanged += new System.EventHandler(this.cbx1_CheckedChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Decimal";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(27, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Binary";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(27, 127);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Hexadecimal";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(27, 155);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Ascii";
+            // 
+            // lbloutdec
+            // 
+            this.lbloutdec.AutoSize = true;
+            this.lbloutdec.Location = new System.Drawing.Point(122, 74);
+            this.lbloutdec.Name = "lbloutdec";
+            this.lbloutdec.Size = new System.Drawing.Size(0, 13);
+            this.lbloutdec.TabIndex = 20;
+            // 
+            // lbloutbin
+            // 
+            this.lbloutbin.AutoSize = true;
+            this.lbloutbin.Location = new System.Drawing.Point(122, 100);
+            this.lbloutbin.Name = "lbloutbin";
+            this.lbloutbin.Size = new System.Drawing.Size(0, 13);
+            this.lbloutbin.TabIndex = 21;
+            // 
+            // lblouthex
+            // 
+            this.lblouthex.AutoSize = true;
+            this.lblouthex.Location = new System.Drawing.Point(122, 127);
+            this.lblouthex.Name = "lblouthex";
+            this.lblouthex.Size = new System.Drawing.Size(0, 13);
+            this.lblouthex.TabIndex = 22;
+            // 
+            // lbloutasc
+            // 
+            this.lbloutasc.AutoSize = true;
+            this.lbloutasc.Location = new System.Drawing.Point(122, 155);
+            this.lbloutasc.Name = "lbloutasc";
+            this.lbloutasc.Size = new System.Drawing.Size(0, 13);
+            this.lbloutasc.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 676);
+            this.Controls.Add(this.lbloutasc);
+            this.Controls.Add(this.lblouthex);
+            this.Controls.Add(this.lbloutbin);
+            this.Controls.Add(this.lbloutdec);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cbx1);
+            this.Controls.Add(this.cbx2);
+            this.Controls.Add(this.cbx4);
+            this.Controls.Add(this.cbx8);
+            this.Controls.Add(this.cbx16);
+            this.Controls.Add(this.cbx32);
+            this.Controls.Add(this.cbx64);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbx128);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbx128;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox cbx64;
+        private System.Windows.Forms.CheckBox cbx32;
+        private System.Windows.Forms.CheckBox cbx16;
+        private System.Windows.Forms.CheckBox cbx8;
+        private System.Windows.Forms.CheckBox cbx4;
+        private System.Windows.Forms.CheckBox cbx2;
+        private System.Windows.Forms.CheckBox cbx1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lbloutdec;
+        private System.Windows.Forms.Label lbloutbin;
+        private System.Windows.Forms.Label lblouthex;
+        private System.Windows.Forms.Label lbloutasc;
+    }
+}
+
---
+++ BitflipperV2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace BitflipperV2
+{
+    public partial class Form1 : Form
+    {
+        BitArray bitArray = new BitArray(8);
+
+
+        //Magic:
+        byte ConvertToByte(BitArray bits)
+        {
+            byte currentval = 0;
+            
+            for (byte i = 0; i < 8; i++)
+            {
+                if (bits[i] == true)
+                {
+                    currentval += (byte)(1 << i);
+                }
+            }
+            return currentval;
+        }
+
+        public void DisplayOutput(byte value)
+        {
+            lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            
+            lbloutbin.Text = Convert.ToString(value, 2).PadLeft(8, '0');
+            lblouthex.Text = Convert.ToString(value, 16).ToUpper();
+            lbloutasc.Text = Convert.ToChar(value).ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbx128_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[7] = cbx128.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+            
+            
+            /*lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            lbloutasc.Text = Convert.ToChar(currentval).ToString();)*/
+        }
+
+        private void cbx64_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[6] = cbx64.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx32_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[5] = cbx32.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx16_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[4] = cbx16.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx8_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[3] = cbx8.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx4_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[2] = cbx4.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx2_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[1] = cbx2.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval,2 ).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+
+        private void cbx1_CheckedChanged(object sender, EventArgs e)
+        {
+            bitArray[0] = cbx1.Checked;
+            DisplayOutput(ConvertToByte(bitArray));
+
+            //lbloutdec.Text = ConvertToByte(bitArray).ToString();
+            //lbloutbin.Text = Convert.ToString(currentval, 2).PadLeft(8, '0');
+            //lblouthex.Text = Convert.ToString(currentval, 16).ToUpper();
+            //lbloutasc.Text = Convert.ToChar(currentval).ToString();
+        }
+    }
+}
